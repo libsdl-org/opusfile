9e5322c62de32d2b2ad88324b3ad311174cec41d
Ozkan Sezer <sezeroz@gmail.com>
2026-04-23 00:56:10 +0300

    Android.mk: remove libopusurl sources

--- Android.mk
+++ Android.mk
@@ -10,13 +10,11 @@ LOCAL_C_INCLUDES := \
 LOCAL_CFLAGS := -DOPUS_BUILD -DHAVE_LRINTF -DFLOATING_POINT -DFLOAT_APPROX -DUSE_ALLOCA
 
 LOCAL_SRC_FILES += \
-    src/http.c \
     src/info.c \
     src/internal.c \
     src/opusfile.c \
-    src/stream.c \
-    src/wincerts.c
+    src/stream.c
 
-LOCAL_SHARED_LIBRARIES := opus
+LOCAL_SHARED_LIBRARIES := opus ogg
 
 include $(BUILD_STATIC_LIBRARY)
